ef1eab2f4dc10bcc4ec0a86dcf78227561a4455e
Prakashini Pattabiraman <prakashini.pattabiraman@capgemini.com>
2020-05-12 20:06:33 +0530

    checking in auth changes

---
+++ PhotoFlick/Network/APIHelper/OauthService.swift
@@ -0,0 +1,15 @@
+//
+//  BaseService.swift
+//  PhotoFlick
+//
+//  Created by Prakashini Pattabiraman on 12/05/20.
+//  Copyright © 2020 Shabana Sheik. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+
+class OauthService {
+    var oauthswift: OAuthSwift?
+    static let shared = OauthService()
+}
